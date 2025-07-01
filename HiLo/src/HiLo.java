87f204221cfbe9d2daff532c0a1c2f9e4069a9b6
stmcpeters <stmcpeters@gmail.com>
2025-06-30 17:09:10 -0700

    Add initial implementation of HiLo game and README documentation

---
+++ HiLo/src/HiLo.java
@@ -0,0 +1,16 @@
+
+public class HiLo {
+    public static void main(String[] args) throws Exception {
+        // generate random number using math library
+            // will have to multiply math.random by 101 to get 0-100
+            // save number to variable
+            // Notes: math.random() returns 0.0 to < 1.0; will have to type cast to int value
+        // prompt for user to guess a number from 1-100
+            // create new scanner object to hold the user's number
+            // save user's guess to variable
+        // compare random number to user's guess
+        // display if the number is higher/lower/equal
+            // if numbers not equal have user guess until correct -- while loop?
+        // ask user to play again after winning    
+    }
+}
