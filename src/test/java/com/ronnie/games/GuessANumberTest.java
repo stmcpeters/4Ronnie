3844910c802cd9b4fbf950afbf4bf6234192cc6d
stmcpeters <stmcpeters@gmail.com>
2025-07-02 14:02:33 -0700

    Add initial test classes for Guess A Number and Main

---
+++ src/test/java/com/ronnie/games/GuessANumberTest.java
@@ -0,0 +1,10 @@
+package com.ronnie.games;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GuessANumberTest {
+    
+    // TODO: Add tests for GuessANumber class
+    
+}
---
+++ src/test/java/com/ronnie/games/MainTest.java
@@ -0,0 +1,10 @@
+package com.ronnie.games;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    
+    // TODO: Add tests for Main class
+    
+}
